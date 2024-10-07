d62a7305918f3f22149c95c186f0afe590320fd6
Mtanash <tanashkoo@gmail.com>
2024-10-07 23:42:34 +0300

    add summarize text type

--- src/api/mutations/summarizeText.ts
+++ src/api/mutations/summarizeText.ts
@@ -1,6 +1,12 @@
 import axiosInstance from "../axios/axiosInstance";
 
-export const summarizeText = async (text: string) => {
+interface SummarizeResponse {
+  summary: string;
+}
+
+export const summarizeText = async (
+  text: string
+): Promise<SummarizeResponse> => {
   const response = await axiosInstance.post("/text/summarize", { text });
   return response.data;
 };
